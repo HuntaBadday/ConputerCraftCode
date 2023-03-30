0597ff84e3c76d08cfbca32acac90fd676d69702
HuntaBadday <65245761+HuntaBadday@users.noreply.github.com>
2023-03-30 17:33:08 -0300

    Update internetTest1.lua

--- Internet/internetTest1.lua
+++ Internet/internetTest1.lua
@@ -1,3 +1,3 @@
 print(shell.dir)
 
-shell.run("wget https://github.com/HuntaBadday/ConputerCraftCode/raw/main/internet/EthernetDriver/main.lua /internet/ethernet.lua")
+shell.run("wget https://github.com/HuntaBadday/ConputerCraftCode/raw/main/internet/EthernetDriver/main.lua ./ethernet.lua")
