d7dba3eb29ff5fc27a5d99750f04ed2c8b67d254
HuntaBadday <65245761+HuntaBadday@users.noreply.github.com>
2023-03-29 22:05:05 -0300

    Update comp2.lua

--- test/comp2.lua
+++ test/comp2.lua
@@ -1,11 +1,11 @@
 local modem = peripheral.find("modem") or error("No modem found")
 
-local num = 0
-while(1)
+while(true)
 do
     local event, side, channel, replyChannel, message, distance
     repeat
         event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
     until channel == 2
-    print(message)
+    print(tostring(message))
+    coroutine.yield()
 end
