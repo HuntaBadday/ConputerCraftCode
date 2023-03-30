1be13a942710dacafbdd8a93a9d7af2208fb30c4
HuntaBadday <65245761+HuntaBadday@users.noreply.github.com>
2023-03-29 21:54:21 -0300

    a

--- test/comp1.lua
+++ test/comp1.lua
@@ -1,1 +1,8 @@
-print("Hello")
+local modem = peripheral.find("modem") or error("No modem found")
+
+local num = 0
+while(1)
+do
+    modem.transmit(2, 3, "Testing "..tostring(num))
+    num += 1
+end
--- test/comp2.lua
+++ test/comp2.lua
@@ -1,1 +1,11 @@
-print("Hello2")
+local modem = peripheral.find("modem") or error("No modem found")
+
+local num = 0
+while(1)
+do
+    local event, side, channel, replyChannel, message, distance
+    repeat
+        event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
+    until channel == 2
+    print(message)
+end
