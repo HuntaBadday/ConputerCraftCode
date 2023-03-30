c7da05b640e5750c16bc3d6dd057b7e0b8bcdf4d
HuntaBadday <65245761+HuntaBadday@users.noreply.github.com>
2023-03-29 22:01:21 -0300

    Update comp1.lua

--- test/comp1.lua
+++ test/comp1.lua
@@ -1,7 +1,7 @@
 local modem = peripheral.find("modem") or error("No modem found")
 
 local num = 0
-while(true)
+while(true) do
     modem.transmit(2, 3, "Testing "..tostring(num))
     os.sleep(0.05)
 end
