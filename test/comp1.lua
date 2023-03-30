8ef443899674e977f5cbf824fbed6af77b88c178
HuntaBadday <65245761+HuntaBadday@users.noreply.github.com>
2023-03-29 22:38:27 -0300

    Update comp1.lua

--- test/comp1.lua
+++ test/comp1.lua
@@ -3,6 +3,7 @@ local modem = peripheral.find("modem") or error("No modem found")
 local num = 0
 while(true)
 do
+    print("6")
     modem.transmit(2, 3, "Testing "..tostring(num))
     num = num + 1
     coroutine.yield()
